6400748ef8db3f5bc33821da3d1eb7231c9b8329
hiep1997xz <73546080+hiep1997xz@users.noreply.github.com>
2022-02-23 17:04:07 +0700

    feature/addForm

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 const AddEmployee = () => {
   return (
-    <div>AddEmployee</div>
-  )
-}
+    <Form>
+      <Form.Group style={{ marginBottom: 15 }}>
+        <Form.Control type="text" placeholder="Name" name="name" />
+      </Form.Group>
+      <Form.Group style={{ marginBottom: 15 }}>
+        <Form.Control type="email" placeholder="Email" name="email" />
+      </Form.Group>
+      <Form.Group style={{ marginBottom: 15 }}>
+        <Form.Control as="textarea" placeholder="Address" name="address" />
+      </Form.Group>
+      <Form.Group style={{ marginBottom: 15 }}>
+        <Form.Control type="text" placeholder="Phone" name="phone" />
+      </Form.Group>
+      <Button
+        className="btn btn-success"
+        type="submit"
+        style={{ width: "100%" }}
+      >
+        Add New Employee
+      </Button>
+    </Form>
+  );
+};
 
-export default AddEmployee
+export default AddEmployee;
